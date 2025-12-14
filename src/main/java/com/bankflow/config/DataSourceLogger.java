cdfa096f64f57f0a4818137794369ff32dd1d7c8
lenamargariann <lena.margaryan.2001@gmail.com>
2025-12-14 04:22:54 +0400

    Refactor DataSourceLogger to use ApplicationEnvironmentPreparedEvent for logging datasource configuration

--- src/main/java/com/bankflow/config/DataSourceLogger.java
+++ src/main/java/com/bankflow/config/DataSourceLogger.java
@@ -1,32 +1,29 @@
 package com.bankflow.config;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.context.event.ApplicationReadyEvent;
-import org.springframework.context.event.EventListener;
+import org.springframework.boot.context.event.ApplicationEnvironmentPreparedEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 
 @Slf4j
 @Component
-public class DataSourceLogger {
+public class DataSourceLogger implements ApplicationListener<ApplicationEnvironmentPreparedEvent> {
 
-    @Value("${spring.datasource.url:NOT_SET}")
-    private String datasourceUrl;
+    @Override
+    public void onApplicationEvent(ApplicationEnvironmentPreparedEvent event) {
+        Environment env = event.getEnvironment();
 
-    @Value("${spring.datasource.username:NOT_SET}")
-    private String datasourceUsername;
+        String dbUrlEnv = env.getProperty("DB_URL", "NOT_SET");
+        String dbUsernameEnv = env.getProperty("DB_USERNAME", "NOT_SET");
+        String datasourceUrl = env.getProperty("spring.datasource.url", "NOT_SET");
+        String datasourceUsername = env.getProperty("spring.datasource.username", "NOT_SET");
+        String activeProfile = String.join(", ", env.getActiveProfiles());
 
-    @Value("${DB_URL:NOT_SET}")
-    private String dbUrlEnv;
-
-    @Value("${DB_USERNAME:NOT_SET}")
-    private String dbUsernameEnv;
-
-    @EventListener(ApplicationReadyEvent.class)
-    public void logDataSourceConfiguration() {
         log.info("========================================");
-        log.info("DATASOURCE CONFIGURATION");
+        log.info("DATASOURCE CONFIGURATION (Early Startup)");
         log.info("========================================");
+        log.info("Active Profile: {}", activeProfile.isEmpty() ? "default" : activeProfile);
         log.info("Environment Variables:");
         log.info("  DB_URL env var: {}", dbUrlEnv);
         log.info("  DB_USERNAME env var: {}", dbUsernameEnv);
